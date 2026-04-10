110896c5d0f842a1157c8dc35ff78b2c112719c5
Goutham Gollapalli <124399318+GG167@users.noreply.github.com>
2026-04-10 09:34:55 +0530

    init

---
+++ script.py
@@ -0,0 +1,8 @@
+import flask, flask_cors, flask_socketio, requests
+import speech_recognition as sr, pyaudio, pydub
+import yagmail, imapclient
+import bcrypt, cryptography
+import sqlalchemy, mysql.connector
+import numpy, pandas
+
+print("All imports successful!")
